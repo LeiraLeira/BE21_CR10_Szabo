432380a0a290f6ed07d0f08d93b3085913f3a01c
Ariel Szabó <arielszabo@outlook.com>
2021-04-23 17:31:21 +0200

    Version3

--- actions/a_create.php
+++ actions/a_create.php
@@ -3,22 +3,45 @@ require_once 'db_connect.php';
 require_once 'file_upload.php';
 
 if ($_POST) {   
-    $name = $_POST['name'];
-    $price = $_POST['price'];
+    // $id = $_POST['id'];
+    // $isbn_code = $_POST['isbn_code'];
+    $title = $_POST['title'];
+    $author_first_name = $_POST['author_first_name'];
+    $author_last_name = $_POST['author_last_name'];
+    $short_description = $_POST['short_description'];
+    $publisher_name = $_POST['publisher_name'];
+    $publisher_size = $_POST['publisher_size'];
+    $publisher_address = $_POST['publisher_address'];
+
+    // $publish_date = $_POST['publish_date'];
+    // $publish_date = date('Y-m-d', strtotime($_POST['publish_date'])) ;
+
+
+    $media_type = $_POST['media_type'];
+    $status = $_POST['status'];
+
+
     $uploadError = '';
     //this function exists in the service file upload.
-    $picture = file_upload($_FILES['picture']);  
+    $image = file_upload($_FILES['image']);  
    
-    $sql = "INSERT INTO products (name, price, picture) VALUES ('$name', $price, '$picture->fileName')";
+    $sql = "INSERT INTO media (title, author_first_name, author_last_name, short_description, publisher_name, publisher_size, publisher_address, media_type, status, image) VALUES ('$title', '$author_first_name', '$author_last_name', 'short_description', '$publisher_name', '$publisher_size', '$publisher_address', '$media_type', '$status', '$image->fileName')";
 
     if ($connect->query($sql) === true) {
         $class = "success";
         $message = "The entry below was successfully created <br>
             <table class='table w-50'><tr>
-            <td> $name </td>
-            <td> $price </td>
+            <td> $title </td>
+            <td> $author_first_name </td>
+            <td> $author_last_name </td>
+            <td> $short_description </td>
+            <td> $publisher_name </td>
+            <td> $publisher_size </td>
+            <td> $publisher_address </td>
+            <td> $media_type </td>
+            
             </tr></table><hr>";
-        $uploadError = ($picture->error !=0)? $picture->ErrorMessage :'';
+        $uploadError = ($image->error !=0)? $image->ErrorMessage :'';
     } else {
         $class = "danger";
         $message = "Error while creating record. Try again: <br>" . $connect->error;
--- create.php
+++ create.php
@@ -15,21 +15,66 @@
     </head>
     <body>
         <fieldset>
-            <legend class='h2'>Add Product</legend>
+            <legend class='h2'>Add Media</legend>
             <form action="actions/a_create.php" method= "post" enctype="multipart/form-data">
                 <table class='table'>
+
+                    <!-- <tr>
+                        <th>Id</th>
+                        <td><input class='form-control' type="number" name= "id" placeholder="ID" step="any" /></td>
+                    </tr>
                     <tr>
-                        <th>Name</th>
-                        <td><input class='form-control' type="text" name="name"  placeholder="Product Name" /></td>
+                        <th>ISBN Code</th>
+                        <td><input class='form-control' type="number" name= "isbn_code" placeholder="ISBN Code" step="any" /></td>
+                    </tr> -->
+                    <tr>
+                        <th>Title</th>
+                        <td><input class='form-control' type="text" name="title"  placeholder="Title" /></td>
+                    </tr>
+                    <tr>
+                        <th>Author_first_name</th>
+                        <td><input class='form-control' type="text" name="author_first_name"  placeholder="Authors firstname" /></td>
+                    </tr>
+    
+                    <tr>
+                        <th>Author_last_name</th>
+                        <td><input class='form-control' type="text" name="author_last_name"  placeholder="Authors lastname" /></td>
                     </tr>    
                     <tr>
-                        <th>Price</th>
-                        <td><input class='form-control' type="number" name= "price" placeholder="Price" step="any" /></td>
-                    </tr>
+                        <th>Short_description</th>
+                        <td><input class='form-control' type="text" name="short_description"  placeholder="Short description" /></td>
+                    </tr>    
                     <tr>
-                        <th>Picture</th>
-                        <td><input class='form-control' type="file" name="picture" /></td>
+                        <th>Publisher_name</th>
+                        <td><input class='form-control' type="text" name="publisher_name"  placeholder="Publishers name" /></td>
+                    </tr>    
+                    <tr>
+                        <th>Publisher_size</th>
+                        <td><input class='form-control' type="text" name="publisher_size"  placeholder="Publishers size" /></td>
+                    </tr>    
+                    <tr>
+                        <th>Publisher_address</th>
+                        <td><input class='form-control' type="text" name="publisher_address"  placeholder="Publishers address" /></td>
+                    </tr>    
+                    <!-- <tr>
+                        <th>Publish_date</th>
+                        <td><input class='form-control' type="date" name="publish_date"  placeholder="Publish date" /></td>
+                    </tr>     -->
+                    <tr>
+                        <th>Media_type</th>
+                        <td><input class='form-control' type="text" name="media_type"  placeholder="Media type" /></td>
+                    </tr>    
+                    <tr>
+                        <th>Status</th>
+                        <td><input class='form-control' type="text" name="status"  placeholder="Status" /></td>
+                    </tr>  
+
+
+                    <tr>
+                        <th>Image</th>
+                        <td><input class='form-control' type="file" name="image" /></td>
                     </tr>
+
                     <tr>
                         <td><button class='btn btn-success' type="submit">Insert Product</button></td>
                         <td><a href="index.php"><button class='btn btn-warning' type="button">Home</button></a></td>
