f23322d80ee85ebbfa1b051705fc32e2d4bea099
Ariel Szabó <arielszabo@outlook.com>
2021-04-24 12:52:26 +0200

    Version400

--- create.php
+++ create.php
@@ -14,6 +14,42 @@
         </style>
     </head>
     <body>
+
+
+    <nav class="navbar navbar-expand-lg navbar-light" >
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">BIG LIBRARY</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNavDropdown">
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="home.php">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="index.php">Show Media</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="create.php">Add Media</a>
+        </li>
+        <li class="nav-item" style="margin-right: 10rem;">
+          <a class="nav-link"  href="contact.php">Contact Us</a>
+        </li>
+
+      </form>
+      </ul>
+    </div>
+  </div>
+</nav>
+
+
+
+
+
+
+
+
         <fieldset>
             <legend class='h2'>Add Media</legend>
             <form action="actions/a_create.php" method= "post" enctype="multipart/form-data">
